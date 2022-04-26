426a92cc48966df331a5632e70957d034cd75694
rx570rohit <101522761+rx570rohit@users.noreply.github.com>
2022-04-26 18:03:12 +0530

    Add project files.

---
+++ LadderSnake.cs
@@ -0,0 +1,59 @@
+﻿using System;
+
+namespace SnakeLadder
+{
+
+    public class LadderSnake
+    {
+         static int playerPos = 0;
+        static Random DieRoll = new Random();
+
+       static  Random SnakeVsLadder = new Random();
+        public static  void snakeVsLadder()
+        {
+
+            while (true)
+            {
+                if (playerPos <= 100)
+                {
+
+                    int v = SnakeVsLadder.Next(0, 2);
+
+                    if (v == 1)
+                    {
+                        if (playerPos <= 100) playerPos += DieRoll.Next(1, 7);
+                    }
+                    else
+                    {
+                        if (playerPos <= 100)
+                        {
+                            playerPos = playerPos - DieRoll.Next(1, 7);
+                        }
+                    }
+
+                }
+                else break;
+                Console.WriteLine(playerPos);
+            }
+            if (playerPos == 100)
+            {
+                    Console.WriteLine("WON");
+
+            }
+
+
+                Console.WriteLine(playerPos);
+            // else { snakeVsLadder(); }
+
+        }
+        public static void Main(String[] arg)
+        {
+            
+
+            
+            snakeVsLadder();
+            
+            
+        }
+    }
+}
